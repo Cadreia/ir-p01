57d698552b3d3fd8b432c57c07367895ee76199b
Audrey Ndum <audreyndum@gmail.com>
2023-10-26 22:55:04 +0200

    refactor

--- src/ir/exercise/p01/AppMain.java
+++ src/ir/exercise/p01/AppMain.java
@@ -4,43 +4,29 @@ import ir.exercise.p01.a.StandardTokenizerEx;
 import ir.exercise.p01.a.WhitespaceTokenizerEx;
 import ir.exercise.p01.b.StopwordFilterEx;
 import ir.exercise.p01.c.CustomAnalyzerEx;
-import ir.exercise.p01.c.CustomAnalyzerExtra;
-import org.apache.lucene.analysis.Analyzer;
 
 public class AppMain {
     public static void main(String[] args) throws Exception {
         String text = LuceneConstants.text;
 
+        System.out.println("======================Standard Tokenizer========================");
         // Print tokens from Standard Tokenizer
         StandardTokenizerEx standardTokenizer = new StandardTokenizerEx();
         standardTokenizer.printTokens(text);
 
-        System.out.println("1==============================================");
-
-
+        System.out.println("===================Whitespace Tokenizer=========================");
         // Print tokens from Whitespace Tokenizer
         WhitespaceTokenizerEx whitespaceTokenizer = new WhitespaceTokenizerEx();
         whitespaceTokenizer.printTokens(text);
 
-        System.out.println("2==============================================");
-
-
+        System.out.println("============Standard Tokenizer and Stopword Filter==============");
         // Print tokens after applying Standard Tokenizer and Stopword Filter
         StopwordFilterEx stopwordFilter = new StopwordFilterEx();
         stopwordFilter.printTokens(text);
 
-        System.out.println("3==============================================");
-
-
-        // Print tokens after applying custom analyzer
+        System.out.println("======================Custom Analyzer==========================");
+        // Print tokens after applying Custom Analyzer
         CustomAnalyzerEx customAnalyzer = new CustomAnalyzerEx();
         customAnalyzer.printTokens(text);
-
-        System.out.println("4==============================================");
-
-
-        // Print tokens after applying custom analyzer extra
-        CustomAnalyzerExtra customAnalyzerExtra = new CustomAnalyzerExtra();
-        customAnalyzerExtra.printTokens(text);
     }
 }
